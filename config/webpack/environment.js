220e5a5b2cec609565ee880fcb687c7c471ce1bb
Y M <ym@YM.local>
2020-06-23 18:53:15 +0900

    add bootstrap,fontawesome

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+const webpack = requier('webpack')
+
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugins({
+    $: 'jquery/scr/jquery',
+    jQuery: 'jquery/scr/jquery'
+  })
+)
