1769d1aec62653540599e157e5704179af83245f
Y M <ym@YM.local>
2020-06-24 06:04:49 +0900

    修正成功 webpacker 再インストール

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
-
-const webpack = requier('webpack')
-
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugins({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
