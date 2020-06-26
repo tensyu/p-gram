f05fb6dcd06f9040f69c61c54c741edd8fa430e1
Y M <ym@YM.local>
2020-06-26 21:40:22 +0900

    add new create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+  
+  privete
+  
+  def post_params
+    params.require(:post).permit(:caption)
+  end
 end
+
