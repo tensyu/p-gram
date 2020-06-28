2263e28063d5bf2292792accdeeb9c475e74b94d
Y M <ym@YM.local>
2020-06-28 20:07:29 +0900

    add user関係

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :set_search
+
+  # def set_search
+  #   @q = Post.ransack(params[:q])
+  #   @posts = @q.result.find_newest_post(params[:page])
+  #              .with_user_and_comment
+  # end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :description, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :description, :avatar])
+  end
 end
+
