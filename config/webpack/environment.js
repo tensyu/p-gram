3ebe0d0fde8e18e7ac5b9a02c1429a7a11cf324f
Y M <ym@YM.local>
2020-06-29 22:08:54 +0900

    bootstrap設定修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+const webpack = require('webpack')
+
+environment.plugins.prepend('provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
