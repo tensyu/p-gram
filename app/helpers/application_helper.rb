294cecc0902bd181e1c98488a9779860aaec8359
Y M <ym@YM.local>
2020-07-02 20:47:07 +0900

    検索機能修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
 
   def user_icon_link(class_name)
-    <% if user_signed_in? %>
-      <%= link_to user_path(current_user), class: class_name do %>
-        <i class="far fa-user"></i>
-      <% end %>
-    <% else %>
-      <%= link_to new_user_session_path, class: class_name do %>
-        <i class="far fa-user"></i>
-      <% end %>
-    <% end %>
+    if user_signed_in?
+      link_to user_path(current_user), class: class_name do
+        content_tag(:i, "", class: "far fa-user")
+      end
+    else
+      link_to new_user_session_path, class: class_name do
+        content_tag(:i, "", class: "far fa-user")
+      end
+    end
   end
 end
