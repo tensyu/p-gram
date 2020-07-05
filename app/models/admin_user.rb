04cf82230815c15d1ef8014cc86f5d2c576c95a4
tensyu <tensyu31@gmail.com>
2020-07-05 15:53:55 +0900

    win7

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,7 +2,7 @@ class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
-        #  :registerable,
+         :registerable,
         #  :recoverable, 
          :rememberable, 
          :validatable
